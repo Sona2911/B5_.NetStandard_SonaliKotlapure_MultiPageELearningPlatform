1070c8cdafad9e3fef2493017395d16be571f6ae
User <user@example.com>
2026-03-19 01:53:28 +0530

    Fixed storage script loading

--- js/storage.js
+++ js/storage.js
@@ -1,45 +1,6 @@
 
-/* ======================================
-   COMPLETE COURSE FUNCTION
-   Saves completed course + updates count
-====================================== */
 
-// function completeCourse(courseName){
 
-// Get stored completed courses
-// let completedCourses = JSON.parse(localStorage.getItem("completedCourses")) || [];
-
-// Check if course already completed
-// if(!completedCourses.includes(courseName)){
-
-// completedCourses.push(courseName);
-
-// Save updated list
-// localStorage.setItem("completedCourses", JSON.stringify(completedCourses));
-
-// alert("🎉 " + courseName + " course marked as completed!");
-
-// }
-// else{
-
-// alert("✅ You already completed this course.");
-
-// }
-
-// Update UI count if dashboard element exists
-// let courseElement = document.getElementById("completedCourses");
-
-// if(courseElement){
-
-// courseElement.innerText = completedCourses.length;
-
-// }
-
-// }
-
-/* ======================================
-   COMPLETE COURSE FUNCTION
-====================================== */
 
 function completeCourse(courseName){
 
